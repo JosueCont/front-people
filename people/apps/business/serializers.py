8919956759f07f527a9afb59d8bd2ae74f578cca
cuicab <jcarlosuicab1995@gmail.com>
2020-12-17 17:31:36 -0600

    Ajustes serializer de nodos.

--- people/apps/business/serializers.py
+++ people/apps/business/serializers.py
@@ -2,10 +2,22 @@ from people.apps.business import models
 from rest_framework import serializers
 
 
+class ChildNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Node
+        fields = "__all__"
+
+
 class NodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Node
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super(NodeSerializer, self).to_representation(instance)
+        if instance.parent:
+            representation['parent'] = ChildNodeSerializer(instance=instance.parent).data
+        return representation
 
 
 class NodePersonSerializer(serializers.ModelSerializer):
