e4746513fcfb58121e28a09d092bfe0554ef566e
cuicab <jcarlosuicab1995@gmail.com>
2020-12-22 14:00:08 -0600

    Ajustes, se le agrega la opción de LMS.

--- components/CardApps.jsx
+++ components/CardApps.jsx
@@ -1,6 +1,7 @@
-import { Card, message, Space } from "antd";
-import { BankOutlined, PlusOutlined, TeamOutlined } from "@ant-design/icons";
+import {Card, Col, message, Space} from "antd";
+import { BankOutlined, PlusOutlined, TeamOutlined, VideoCameraOutlined } from "@ant-design/icons";
 import Router from "next/router";
+import Link from "next/link";
 
 const { Meta } = Card;
 
@@ -8,6 +9,11 @@ const cardApps = () => {
   const openApp = (appName) => {
     Router.push("/" + appName);
   };
+
+  const openExternalUrl = (url) => {
+     window.location.href = url
+  };
+
 
   const messageAdd = () => message.success("Agregar nueva App");
 
@@ -27,13 +33,21 @@ const cardApps = () => {
             />
             <span>Business</span>
           </div>
-          <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ display: "flex", flexDirection: "column", marginLeft: "15px"}}>
             <TeamOutlined
               hoverable={true}
               onClick={() => openApp("home")}
               style={{ fontSize: "40px" }}
             />
             <span>People</span>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", marginLeft: "15px"  }}>
+            <VideoCameraOutlined
+                hoverable={true}
+                onClick={() => openExternalUrl("https://demo.lms.ddns.me")}
+                style={{ fontSize: "40px"}}
+            />
+            <span>LMS</span>
           </div>
         </Space>
       </Card>
