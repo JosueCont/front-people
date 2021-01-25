b5ef6ef716b42cebf0c5db5a389f19532f7ae225
= <pedro.leon@hiumanlab.com>
2021-01-25 13:41:31 -0600

    se agrega endpoint para obtener los puestos de un departamento

--- people/apps/business/views.py
+++ people/apps/business/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from people.apps.business import serializers
 from people.apps.business.models import Node, NodePerson, Department, JobDepartment
+from people.apps.person.serializers import JobSerializer
 
 
 class NodeViewSet(viewsets.ModelViewSet):
@@ -20,6 +24,23 @@ class DepartmentViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.DepartmentSerializer
     queryset = Department.objects.all()
     filterset_fields = ('id',)
+
+    @action(detail=True, methods=['get'])
+    def job_for_department(self, request, pk):
+
+        if pk:
+            try:
+                department = self.get_object()
+                job_department = JobDepartment.objects.filter(department=department)
+                if job_department:
+                    array_jobs = []
+                    for job in job_department:
+                        array_jobs.append(JobSerializer(job.job).data)
+                return Response(data=array_jobs, status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response(data={'message': 'No se encontraron datos'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(data={"id requerido"}, tatus=status.HTTP_400_BAD_REQUEST)
 
 
 class JobDepartmentViewSet(viewsets.ModelViewSet):
