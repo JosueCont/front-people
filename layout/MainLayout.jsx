43e9502d1d4e9e1a4bebed4010000ab282aecf9b
irvin <irvin.albornoz@hiumanlab.com>
2021-01-29 10:47:57 -0600

    header

--- layout/MainLayout.jsx
+++ layout/MainLayout.jsx
@@ -14,7 +14,7 @@ const MainLayout = ({ ...props }) => {
   return (
     /* <IntlProvider locale={state.lang} messages={langMessages[state.lang]}> */
     <Layout className="layout">
-      
+      <HeaderCustom key="custom_header" currentKey={props.currentKey} />
       <Content key="content"
         className="site-layout"
         style={{ padding: "30px 50px" }}
