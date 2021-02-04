ee764c9e4bf798d4ea4a0001189d5c3487f7116b
= <pedro.leon@hiumanlab.com>
2021-02-04 12:17:29 -0600

    correccion de links en config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 export const API_URL = process.browser
   ? process.env.NEXT_PUBLIC_API_BASE_URL
   : process.env.API_BASE_URL;
-// export const LOGIN_URL = "https://khonnect.hiumanlab.com";
-export const LOGIN_URL = "http://localhost:3000"
-// export const APP_ID = "5fa42a1ca6f5f821bbe7fbea"; ///Id de la app People
-export const APP_ID = "5f417a53c37f6275fb614104"; ///Id de la app People local
+export const LOGIN_URL = "https://khonnect.hiumanlab.com";
+// export const LOGIN_URL = "http://localhost:3000"
+export const APP_ID = "5fa42a1ca6f5f821bbe7fbea"; ///Id de la app People
+// export const APP_ID = "5f417a53c37f6275fb614104"; ///Id de la app People local
