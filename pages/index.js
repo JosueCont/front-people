4a97e25b9383b4a4a680668c85446d1a12b43022
hiuman <jasson.acea@hiumanlab.com>
2021-02-12 11:44:41 -0600

    Validaciones de formularios

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,36 @@
 import Head from "next/head";
 import React, { useEffect } from "react";
 import LoginForm from "../components/LoginForm";
-import {  
-  Row,
-  Col, 
-  Card
-} from "antd";
-
+import { Row, Col, Card } from "antd";
 
 const Home = () => {
   return (
     <>
-      <Col span={24}  className="containerPrincipal">
+      <Col span={24} className="containerPrincipal">
         <Row justify="space-around" align="middle">
-        <Col xl={12} lg={12} md={12} sm={12} xs={24}>
-        {/* <img className="card-login" alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> */}
-        </Col>
-        <Col xl={12} lg={12} md={12} sm={12} xs={24} className="loginContainer">
-          <div style={{ textAlign: "center" }}>
-            <h1 className="font-color-khor">Welcome to Concierge</h1>
-            <p className="font-color-khor">
-              A new people management experience
-            </p>
-          </div>
-          <LoginForm />
-        </Col>
-        </Row>       
+          <Col xl={12} lg={12} md={12} sm={12} xs={24}>
+            {/* <img className="card-login" alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" /> */}
+          </Col>
+          <Col
+            xl={12}
+            lg={12}
+            md={12}
+            sm={12}
+            xs={24}
+            className="loginContainer"
+          >
+            <div style={{ textAlign: "center" }}>
+              <img
+                style={{ margin: "auto" }}
+                src={
+                  "https://khorplus.s3.amazonaws.com/demo/people/person/images/photo-profile/12220210623/staff_1-1.png"
+                }
+                alt=""
+              />
+            </div>
+            <LoginForm />
+          </Col>
+        </Row>
       </Col>
     </>
   );
