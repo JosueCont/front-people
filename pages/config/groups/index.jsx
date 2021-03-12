ee7c67fbc0c1de0c1800de33d5eaca4509162193
= <pedro.leon@hiumanlab.com>
2021-03-12 12:47:33 -0600

    Se corrige el mensaje de eliminado de perfiles

--- pages/config/groups/index.jsx
+++ pages/config/groups/index.jsx
@@ -67,15 +67,26 @@ const Groups = () => {
       headers: headers,
     })
       .then(function (response) {
+        console.log("Response", response);
         if (response.status === 200) {
-          message.success({
-            content: "Grupo eliminado exitosamente",
-            className: "custom-class",
-            style: {
-              marginTop: "20vh",
-            },
-          });
-          getGroups();
+          if (response.data.level === "associated") {
+            message.warning({
+              content: "El perfil tiene usuarios asociados",
+              className: "custom-class",
+              style: {
+                marginTop: "20vh",
+              },
+            });
+          } else {
+            message.success({
+              content: "Grupo eliminado exitosamente",
+              className: "custom-class",
+              style: {
+                marginTop: "20vh",
+              },
+            });
+            getGroups();
+          }
         }
       })
       .catch(function (error) {
