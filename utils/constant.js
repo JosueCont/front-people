e993242f3e3e24e11173dc9a760626f38188d247
hiuman <jasson.acea@hiumanlab.com>
2021-08-12 13:58:14 -0500

    Register user

---
+++ utils/constant.js
@@ -0,0 +1,84 @@
+export const genders = [
+  {
+    label: "Masculino",
+    value: 1,
+  },
+  {
+    label: "Femenino",
+    value: 2,
+  },
+  {
+    label: "Otro",
+    value: 3,
+  },
+];
+export const civilStatus = [
+  {
+    label: "Soltero(a)",
+    value: 1,
+  },
+  {
+    label: "Casado(a)",
+    value: 2,
+  },
+  {
+    label: "Viudo(a)",
+    value: 3,
+  },
+];
+export const typePhones = [
+  {
+    label: "Alterno",
+    value: 1,
+  },
+  {
+    label: "Principal",
+    value: 2,
+  },
+  {
+    label: "Recados",
+    value: 3,
+  },
+];
+export const typeLines = [
+  {
+    label: "Celular",
+    value: 1,
+  },
+  {
+    label: "Fijo",
+    value: 2,
+  },
+];
+export const typeStreet = [
+  {
+    label: "Avenida",
+    value: 1,
+  },
+  {
+    label: "Boulevard",
+    value: 2,
+  },
+  {
+    label: "Calle",
+    value: 3,
+  },
+];
+export const periodicity = [
+  {
+    label: "Semanal",
+    value: 1,
+  },
+  {
+    label: "Catorcenal",
+    value: 2,
+  },
+  {
+    label: "Quincenal",
+    value: 3,
+  },
+  {
+    label: "Mensual",
+    value: 4,
+  },
+];
