ae5814798aaeb052692a9e171727f0f71e71ddc9
juan diaz <juan.diaz@hiumanlab.com>
2021-12-02 16:17:46 -0600

    se corrije valdiacion para visualziacion de intranet, se omiten console.log y se agregan algunos iconos

--- components/MainSider.jsx
+++ components/MainSider.jsx
@@ -23,6 +23,7 @@ import {
   BusinessCenterOutlined,
   BusinessOutlined,
   SettingsOutlined,
+  GroupOutlined
 } from "@material-ui/icons";
 
 const { Sider } = Layout;
@@ -311,21 +312,25 @@ const MainSider = ({
           >
             Asignar empresa
           </Menu.Item>
-          {props.config && props.config.intranet_access && (
+          {intranetAccess && (
             <SubMenu
               key="intranet"
               title={<FormattedMessage id="header.intranet" />}
+              icon={ <img className="anticon ant-menu-item-icon icon-intranet" src={"images/Intranet.svg"}/>}
               className="subMainMenu"
             >
+             
               <Menu.Item
                 key="groups"
                 onClick={() => router.push({ pathname: "/intranet/groups" })}
+                icon={<GroupOutlined />}
               >
                 <FormattedMessage id="header.groups" />
               </Menu.Item>
               <Menu.Item
                 key="config"
                 onClick={() => router.push({ pathname: "/intranet/config" })}
+                icon={<SettingsOutlined />}
               >
                 <FormattedMessage id="header.config" />
               </Menu.Item>
