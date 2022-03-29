7a01b6ad3306a4ebebd83680c4658784f3caba8e
Antonio Hau Uicab <antonio.hau@hiumanlab.com>
2022-03-29 10:39:13 -0600

    Avances pantalla de validacion con token

--- pages/validation.jsx
+++ pages/validation.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Spin } from 'antd';
+import { LoadingOutlined, CloseCircleFilled, CheckCircleFilled} from '@ant-design/icons';
 
 const Content = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     min-height: 100vh;
+    background-color: #252837;
 `;
 
 const ContentVertical = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
+`;
+
+const ContentVerify = styled(ContentVertical)`
     & p:last-of-type{
-        font-size: 2rem;
-        animation: blink 3s infinite linear;
+        color: #ffff;
+        font-size: 1.5rem;
+        font-weight: bold;
+        animation: fadein 1s ease-in alternate infinite;
     }
 
-    @keyframes blink {
-        from { opacity: 0 }
-        to { opacity: 1 }   
+    @keyframes fadein {
+        from { opacity: 0; }
+        to { opacity: 1; }   
     }
 `;
 
+const ContentError = styled(ContentVertical)`
+
+`;
+
 const validation = () => {
+    const antIcon = <LoadingOutlined style={{ fontSize: 50, color: "white" }} spin />
     return (
         <Content>
-            <ContentVertical>
-                <p>Probando</p>
+            <ContentVerify>
+                <p><Spin indicator={antIcon}/></p>
                 <p>Verificando los datos</p>
-            </ContentVertical>
+            </ContentVerify>
+            <ContentError>
+                <p><CloseCircleFilled /></p>
+            </ContentError>
+            <ContentError>
+                <p><CheckCircleFilled /></p>
+            </ContentError>
         </Content>
     )
 }
