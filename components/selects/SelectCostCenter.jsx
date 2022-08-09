a8a4de0ce2e19c18c9ea4b52aa7d2a6ec036bab7
Gaspar Dzul <casper088@gmail.com>
2022-08-09 18:22:27 -0500

    Se incluyen las relaciones de centro de costos y etiquetas en persona nomina, puestos, y departamentos

---
+++ components/selects/SelectCostCenter.jsx
@@ -0,0 +1,40 @@
+import { Select, Form } from "antd";
+import { connect } from "react-redux";
+const { Option } = Select;
+
+const SelectCostCenter = ({ multiple=false,name='cost_center', viewLabel='',placeholder='Centro de costos', required = true,cat_cost_center, ...props }) => {
+
+  return (
+    <Form.Item
+      name={name}
+      key="itemCostCenter"
+      label={viewLabel}
+    >
+      <Select
+        showArrow
+        mode={multiple && 'multiple'}
+        style={{ width: "100%" }}
+        placeholder={placeholder}
+        notFoundContent="No se encontraron resultados"
+        showSearch
+        optionFilterProp="children"
+      >
+        {cat_cost_center && cat_cost_center.map((item) => {
+          return (
+              <Option key={item.id} value={item.id}>
+                {item.code}
+              </Option>
+          )
+        })}
+      </Select>
+    </Form.Item>
+  );
+};
+
+const mapState = (state) => {
+  return {
+    cat_cost_center: state.catalogStore.cat_cost_center,
+  };
+};
+
+export default connect(mapState)(SelectCostCenter);
