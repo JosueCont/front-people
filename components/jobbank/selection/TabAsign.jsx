ad0b8ea8b15dfa95ccc724aadc8f138c2f6798cf
Antonio Hau Uicab <antonio.hau@hiumanlab.com>
2023-01-27 17:19:14 -0600

    Fix

--- components/jobbank/selection/TabAsign.jsx
+++ components/jobbank/selection/TabAsign.jsx
@@ -19,7 +19,8 @@ import {
 } from '../../../utils/rules';
 import { SearchOutlined } from '@ant-design/icons';
 import ModalAsignament from './ModalAsignment';
-
+import ListItems from '../../../common/ListItems';
+import WebApiJobBank from '../../../api/WebApiJobBank';
 
 const TabAsign = ({ loading }) => {
 
@@ -27,7 +28,18 @@ const TabAsign = ({ loading }) => {
 
   const closeModal = () =>{
     setOpenModal(false)
-}
+  }
+
+  // const actionDelete = async (id) =>{
+  //   try {
+  //     await WebApiJobBank.deleteVacancyAssesmentCandidateVacancy(id);
+  //     getAssesmets();
+  //     message.success('Evaluación eliminada');
+  //   } catch (e) {
+  //     console.log(e)
+  //     message.error('Evaluación no eliminada');
+  //   }
+  // }
 
   const columns = [
     {
