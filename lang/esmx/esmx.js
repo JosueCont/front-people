cef4d1d826d9236a5745cf9826c3a610efc060e1
Gaspar Dzul <casper088@gmail.com>
2023-05-09 13:37:10 -0600

    Se crea la configuracion para el lenguaje, ya funciona con redux

---
+++ lang/esmx/esmx.js
@@ -0,0 +1,14 @@
+import {generalTex} from './generalText'
+import {errorMessage} from './errorMessages'
+
+
+
+export const esmx = {
+    'config.changeLang':'Change to english',
+    'dashboard.anniversary':'Aniversarios',
+    'dashboard.predominant':'Predominante',
+    'dashboard.birthdaymonth':'Cumpleaños del mes',
+    'dashboard.totalpeople':'Total de personas',
+    ...generalTex,
+    ...errorMessage
+}
