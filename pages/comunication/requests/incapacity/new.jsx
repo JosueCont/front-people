c9c76c37a66c755b93780d5a5046421c59663fff
Antonio Hau Uicab <antonio.hau@hiumanlab.com>
2023-06-02 15:29:18 -0600

    name fix

--- pages/comunication/requests/incapacity/new.jsx
+++ pages/comunication/requests/incapacity/new.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { withAuthSync } from "../../../../libs/auth";
-import AddOrEditInpacity from "../../../../components/comunication/inpacity/AddOrEditInpacity";
+import AddOrEditIncapacity from "../../../../components/comunication/inpacity/AddOrEditIncapacity";
 
 const index = () => {
     return (
-        <AddOrEditInpacity action="add" />
+        <AddOrEditIncapacity action="add" />
     )
 }
 
