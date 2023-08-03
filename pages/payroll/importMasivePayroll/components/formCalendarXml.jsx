2981c41ed488cf1f428b4ba3ac053a866e1db648
Irvin Albornoz <irvin.albornoz@hiumanlab.com>
2023-08-03 17:49:34 -0600

    tipo de impuesto sugerido

--- pages/payroll/importMasivePayroll/components/formCalendarXml.jsx
+++ pages/payroll/importMasivePayroll/components/formCalendarXml.jsx
@@ -6,6 +6,8 @@ import { ruleRequired } from "../../../../utils/rules";
 import locale from "antd/lib/date-picker/locale/es_ES";
 import moment from "moment";
 import { useState } from "react";
+import { connect } from "react-redux";
+import { withAuthSync } from "../../../../libs/auth";
 const { Panel } = Collapse;
 
 const FormCaledanrXml = ({
@@ -67,6 +69,7 @@ const FormCaledanrXml = ({
     ).description;
     setPeriodicityCode(periodicity_code);
     let periodicity_str = paymentPeriodicity.find((item) => item.id == calendar.calendar.periodicity).description
+
     formCalendar.setFieldsValue({
       name: isAddXMLS ? calendar.name : `${calendar.period} - ${periodicity_str}`,
       perception_type: calendar.calendar.perception_type,
@@ -77,6 +80,16 @@ const FormCaledanrXml = ({
       type_tax: isAddXMLS ? calendar.type_tax : calendar.calendar.type_tax,
       salary_days: isAddXMLS ? calendar.salary_days : calendar.calendar.salary_days,
     });
+
+    if(props?.type_tax?.length === 1){
+      formCalendar.setFieldsValue({
+        type_tax: props?.type_tax[0]?.id,
+      })
+      calendar.calendar.type_tax = props?.type_tax[0]?.id
+    }
+
+    
+
     calendar.calendar.name = calendar.name ? calendar.name : `${calendar.period} - ${periodicity_str}`
 
     
@@ -311,4 +324,10 @@ const FormCaledanrXml = ({
   );
 };
 
-export default FormCaledanrXml;
+const mapState = (state) => {
+  return {
+    type_tax: state.fiscalStore.type_tax,
+  };
+};
+
+export default connect(mapState)(withAuthSync(FormCaledanrXml));
