89eff4d42fe4960ddd3a6bbb1c7b49ee96c7fc7e
Gaspar Dzul <casper088@gmail.com>
2023-08-04 12:11:07 -0600

    Ajustes menores e integración de widgets en dashboard

--- lang/esmx/esmx.js
+++ lang/esmx/esmx.js
@@ -8,6 +8,8 @@ export const esmx = {
     'dashboard.anniversary':'Aniversarios',
     'dashboard.predominant':'Predominante',
     'dashboard.birthdaymonth':'Cumpleaños del mes',
+    'dashboard.payrollcalendar':'Calendarios de pago',
+    'dashboard.patronalreg':'Reg. patronal',
     'dashboard.totalpeople':'Total de personas',
     ...generalTex,
     ...errorMessage
